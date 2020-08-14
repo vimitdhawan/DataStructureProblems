85dc10832b43b6935ec70552d229eef82d6da897
Dhawan, Vimit (Agoda) <Vimit.Dhawan@agoda.com>
2020-08-14 23:05:46 +0700

    coin change

--- src/com/vimit/dynamicprogramming/CoinChange.java
+++ src/com/vimit/dynamicprogramming/CoinChange.java
@@ -3,6 +3,7 @@ package com.vimit.dynamicprogramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class CoinChange {
     public static void main(String[] args) throws IOException {
@@ -18,25 +19,23 @@ public class CoinChange {
             for(int t=0; t<strarr.length; t++){
                 arr[t] = Integer.parseInt(strarr[t]);
             }
-            int memo[] = new int[maxSum+1];
-            int count = getCoinCombination(arr, maxSum, memo);
+            int memo[][] = new int[arr.length][maxSum+1];
+            int count = getCoinCombination(arr, maxSum, memo, arr.length);
             System.out.println(count);
 
         }
     }
 
-    private static int getCoinCombination(int[] arr, int maxSum, int[] memo) {
-        if(maxSum==0){
+    private static int getCoinCombination(int[] arr, int maxSum, int[][] memo, int index) {
+        if(maxSum==0)
             return 1;
-        } else if(maxSum<0){
+        if(maxSum<0 || index<=0)
             return 0;
-        } else if(memo[maxSum]!=0){
-            return memo[maxSum];
-        } else{
-            for(int i=0; i<arr.length; i++) {
-            memo[maxSum] = memo[maxSum] + getCoinCombination(arr, maxSum-arr[i], memo);
-            }
-            return memo[maxSum];
+        if(memo[index-1][maxSum]!=0){
+            return memo[index-1][maxSum];
         }
+        memo[index-1][maxSum] =  getCoinCombination(arr, maxSum-arr[index-1], memo, index) + getCoinCombination(arr, maxSum, memo, index-1);
+        return memo[index-1][maxSum];
     }
+
 }
