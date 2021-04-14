fe6df02d4d0b1a80ece389e1db82c0e63c94eba2
Vimit Dhawan <Vimit.Dhawan@agoda.com>
2021-04-14 15:09:10 +0700

    permutation problems

--- src/com/vimit/crackig/coding/dp/CoinChange.java
+++ src/com/vimit/crackig/coding/dp/CoinChange.java
@@ -1,2 +1,43 @@
-package com.vimit.crackig.coding.dp;public class CoinChange {
-}
+package com.vimit.crackig.coding.dp;
+import java.io.*;
+import java.util.*;
+public class CoinChange
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int Arr[] = new int[m];
+            for(int i = 0;i<m;i++)
+                Arr[i] = sc.nextInt();
+            CoinChange ob = new CoinChange();
+            System.out.println(ob.count(Arr,m,n));
+        }
+    }
+
+    public long count(int S[], int m, int n)
+    {
+        long[][] memo = new long[m][n+1];
+        return getCoinChange(S, n, memo, m);
+    }
+
+    public long getCoinChange(int arr[], int maxSum, long[][] memo, int index){
+        if(maxSum==0)
+            return 1;
+        if(maxSum<0 || index<=0)
+            return 0;
+        if(memo[index-1][maxSum]!=0){
+            return memo[index-1][maxSum];
+        }
+        memo[index-1][maxSum] =  getCoinChange(arr, maxSum-arr[index-1], memo, index) + getCoinChange(arr, maxSum, memo, index-1);
+        return memo[index-1][maxSum];
+
+    }
+}    // } Driver Code Ends
+
+
+//User function Template for Java
--- src/com/vimit/dynamicprogramming/PhoneDigit.java
+++ src/com/vimit/dynamicprogramming/PhoneDigit.java
@@ -1,2 +1,72 @@
-package com.vimit.dynamicprogramming;public class PhoneDigit {
+package com.vimit.dynamicprogramming;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class PhoneDigit
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();//testcases
+
+
+        while(t-- > 0)
+        {
+            int n = sc.nextInt(); // input size of array
+            int arr[] = new int[n]; //input the elements of array that are keys to be pressed
+
+            for(int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();//input the elements of array that are keys to be pressed
+            ArrayList <String> res = new Solution().possibleWords(arr, n);
+            for (String i : res) System.out.print (i + " ");
+            System.out.println();
+
+        }
+    }
 }
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static ArrayList <String> possibleWords(int a[], int N)
+    {
+        Map<Integer, String> PhoneMap =  new HashMap<>();
+        PhoneMap.put(2, "abc");
+        PhoneMap.put(3, "def");
+        PhoneMap.put(4, "ghi");
+        PhoneMap.put(5, "jkl");
+        PhoneMap.put(6, "mno");
+        PhoneMap.put(7, "pqrs");
+        PhoneMap.put(8, "tuv");
+        PhoneMap.put(9, "wxyz");
+        ArrayList<String> result = new ArrayList<>();
+        gePossibleWords(PhoneMap, a, "", 0, N, result);
+        ArrayList<String> listWithoutDuplicates = new ArrayList<>(new HashSet<>(result));
+
+        return result;
+
+    }
+
+    static void gePossibleWords( Map<Integer, String> PhoneMap, int a[], String prefix, int current, int N, ArrayList<String> result){
+        if(current == N){
+            result.add(prefix);
+        }else{
+            String value = PhoneMap.get(a[current]);
+            for(char c : value.toCharArray()){
+                gePossibleWords(PhoneMap, a, prefix+c, current+1, N, result);
+
+            }
+
+        }
+
+
+    }
+
+
+}
+
+
